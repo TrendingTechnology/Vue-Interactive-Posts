2bd79f14284b4cc65beb0f38cf73b9a652e13637
Shadi RR <therealshadix@gmail.com>
2019-06-27 14:00:10 +0300

    Update vuetify.js

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,21 @@
-import Vue from "vue";
-import Vuetify from "vuetify/lib";
-import "vuetify/src/stylus/app.styl";
+function onInputValidation(userEmail) {
+    var validEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (document.getElementById(userEmail)) {
+        var emailValue = document.getElementById(userEmail).value;
 
-Vue.use(Vuetify, {
-  iconfont: "md"
-});
+    if (!validEmail.test(emailValue)) {
+        document.getElementById('userEmail').style.border = "1px solid red";
+    }
+    else {
+        document.getElementById('userEmail').style.border = "1px solid transparent";
+    }
+    }else{
+        if (validEmail.test(userEmail)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    return false;
+}
